43e0e216f8fe771d59fc41680b45c02f8ff26f16
doantruongminh <doantruongminh@cryptopie-labo.com>
2024-05-03 14:52:13 +0700

    Initital Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,27 @@
+"use client";
+import { useEffect, useState } from "react";
 import Image from "next/image";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Typography from "@mui/material/Typography";
+import Cookie from "js-cookie";
 
 export default function Home() {
+  const [loggedIn, setLoggedIn] = useState<boolean>(false);
+
+  useEffect(() => {
+    setLoggedIn(!!Cookie.get("TOKEN"));
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
+        <ButtonGroup>
+          <Button href="http://localhost:3000">Home</Button>
+          <Button color={"error"} href="http://localhost:3007">
+            Marketing
+          </Button>
+        </ButtonGroup>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
@@ -15,6 +29,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
+            <Typography color={"yellow"}>Marketing App</Typography>
             By{" "}
             <Image
               src="/vercel.svg"
@@ -26,8 +41,16 @@ export default function Home() {
             />
           </a>
         </div>
+        <ButtonGroup>
+          <Button
+            color={"warning"}
+            href="http://localhost:3001?redirect=http://localhost:3007"
+            style={{ pointerEvents: loggedIn ? "none" : undefined }}
+          >
+            {loggedIn ? "User" : "Login"}
+          </Button>
+        </ButtonGroup>
       </div>
-
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
